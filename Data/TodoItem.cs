aa3f3d0a9e3014ed65a8f31e91e1910215c6c22b
c0m <61923663+c0m@users.noreply.github.com>
2020-08-25 00:35:03 +0100

    Initial commit

---
+++ Data/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hipparu.Data
+{
+    public class TodoItem
+    {
+        public string Titel { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
