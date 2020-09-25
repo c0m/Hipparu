cc609e86a2df1bdf0f2095d49438ecda777ec80d
c0m <61923663+c0m@users.noreply.github.com>
2020-09-25 18:16:30 +0100

    Add root class to AnswerItem

--- Data/AnswerItem.cs
+++ Data/AnswerItem.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 
 namespace Hipparu.Data
 {
+    public class Answers
+    {
+        public IList<AnswerItem> Data { get; set; }
+    }
+
     public class AnswerItem
     {
+        public int Id { get; set; }
         public string RomajiScript { get; set; }
         public string HiraganaScript { get; set; }
         public string KatakanaScript { get; set; }
