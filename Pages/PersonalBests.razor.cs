5d24cb18e2f8f0dbda7e9f756d763f2d9eecaa02
c0m <61923663+c0m@users.noreply.github.com>
2020-10-24 18:43:28 +0100

    Add PersonalBests page

---
+++ Pages/PersonalBests.razor.cs
@@ -0,0 +1,18 @@
+﻿using Hipparu.Data;
+using Newtonsoft.Json;
+using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+using System.Threading.Tasks;
+
+namespace Hipparu.Pages
+{
+
+    public partial class PersonalBests
+    {
+
+    }
+
+}
