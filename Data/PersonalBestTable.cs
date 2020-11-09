78c53560af10433bcc8549f08b614c7a3427724d
c0m <61923663+c0m@users.noreply.github.com>
2020-11-09 02:52:36 +0000

    Create Personal Best Table object

---
+++ Data/PersonalBestTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hipparu.Data
+{
+    public class PersonalBestTable
+    {
+        private TimeSpan[] personalBests;
+
+        private void Initialize()
+        {
+            personalBests = Enumerable.Repeat(TimeSpan.Zero, 5).ToArray();
+        }
+
+        private void NewScore(TimeSpan newScore)
+        {
+            for (int i = 0; i < personalBests.Length; i++)
+            {
+                if(newScore < personalBests[i] || personalBests[i] == TimeSpan.Zero)
+                {
+                    personalBests[i] = newScore;
+                    return;
+                }
+            }
+        }
+
+        private TimeSpan[] GetPersonalBests()
+        {
+            return personalBests;
+        }
+    }
+}
