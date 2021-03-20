ca744263be90024c6919980978bb5bf370355ab9
c0m <61923663+c0m@users.noreply.github.com>
2021-03-20 14:32:10 +0000

    This definitely should have just been done on a separate branch or project. Oh well, trying again.

--- obj/Debug/netstandard2.1/RazorDeclaration/Pages/Index.razor.g.cs
+++ obj/Debug/netstandard2.1/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,91 +13,91 @@ namespace Hipparu.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\Cormac\source\repos\c0m\Hipparu\_Imports.razor"
+#line 1 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Cormac\source\repos\c0m\Hipparu\_Imports.razor"
+#line 2 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Cormac\source\repos\c0m\Hipparu\_Imports.razor"
+#line 3 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Cormac\source\repos\c0m\Hipparu\_Imports.razor"
+#line 4 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Cormac\source\repos\c0m\Hipparu\_Imports.razor"
+#line 5 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\Cormac\source\repos\c0m\Hipparu\_Imports.razor"
+#line 6 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\Cormac\source\repos\c0m\Hipparu\_Imports.razor"
+#line 7 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\Cormac\source\repos\c0m\Hipparu\_Imports.razor"
+#line 8 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\_Imports.razor"
 using Hipparu;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\Cormac\source\repos\c0m\Hipparu\_Imports.razor"
+#line 9 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\_Imports.razor"
 using Hipparu.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\Cormac\source\repos\c0m\Hipparu\Pages\Index.razor"
+#line 2 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\Pages\Index.razor"
 using Hipparu.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\Cormac\source\repos\c0m\Hipparu\Pages\Index.razor"
+#line 3 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\Pages\Index.razor"
 using Plk.Blazor.DragDrop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\Cormac\source\repos\c0m\Hipparu\Pages\Index.razor"
+#line 4 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\Pages\Index.razor"
 using Newtonsoft.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\Cormac\source\repos\c0m\Hipparu\Pages\Index.razor"
+#line 5 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\Pages\Index.razor"
 using System.IO;
 
 #line default
@@ -112,7 +112,7 @@ using System.IO;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 693 "C:\Users\Cormac\source\repos\c0m\Hipparu\Pages\Index.razor"
+#line 693 "C:\Users\Cormac\Source\Repos\c0m\Hipparu\Pages\Index.razor"
  
 
     public void OnGameModeSelect(ChangeEventArgs e)
