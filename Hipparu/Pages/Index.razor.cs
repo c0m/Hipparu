cba642dc58e49417ccf9bd97ad2a8824121e43ec
c0m <61923663+c0m@users.noreply.github.com>
2021-08-13 08:54:18 +0100

    Add ability to swap between menu and game and specify a game mode

--- Hipparu/Pages/Index.razor.cs
+++ Hipparu/Pages/Index.razor.cs
@@ -7,18 +7,67 @@ namespace Hipparu.Pages
 {
     public partial class Index
     {
-        public int gameMode = 0;
+        // Game states:
+        // 0 - Main Menu
+        // 1 - Hiragana Mode
+        // 2 - Katakana Mode
+        // 3 - Mixed Mode
+        private int gameState = 0;
 
-        public void IncrementCount(int mode)
+        // Styles for the game and menu divs to decide visibilty
+        private string menuVisibility = "menu-visible";
+        private string gameVisibility = "game-invisible";
+
+        /// <summary>
+        /// Run PrepareGame() to get the board ready, then switch case to decide which kana to use
+        /// </summary>
+        /// <param name="mode">Int provided by button</param>
+        private void SelectMode(int mode)
         {
-            if (mode == 0)
+            PrepareGame();
+
+            switch (mode)
             {
-                gameMode++;
+                case 1:
+                    //set to display hiragana
+                    break;
+
+                case 2:
+                    //set to display katakana
+                    break;
+
+                case 3:
+                    //enable mixed game switch
+                    break;
+            }
+        }
+        /// <summary>
+        /// Get board prepared for a new game
+        /// </summary>
+        private void PrepareGame()
+        {
+            SwapBetweenGameAndMenu();
+            // Reset the game timer
+            // Populate answer table
+        }
+        /// <summary>
+        /// Swap between menu being visible and game being hidden. This changes the strings of the class of the divs which the menu and games are located in. 
+        /// </summary>
+        private void SwapBetweenGameAndMenu()
+        {
+            if (menuVisibility == "menu-invisible")
+            {
+                gameState = 0;
+                menuVisibility = "menu-visible";
+                gameVisibility = "game-invisible";
             }
             else
             {
-                gameMode = 0;
+                //gameState handled by SelectMode
+                menuVisibility = "menu-invisible";
+                gameVisibility = "game-visible";
             }
         }
     }
+
 }
