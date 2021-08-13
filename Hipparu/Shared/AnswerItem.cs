c5e0bc27b646be4a54a12f1c1be798b5e89e0437
c0m <61923663+c0m@users.noreply.github.com>
2021-08-13 12:52:09 +0100

    Create classes for answers

---
+++ Hipparu/Shared/AnswerItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hipparu.Shared
+{
+    public class Answers
+    {
+        public IList<AnswerItem> Data { get; set; }
+    }
+
+    public class AnswerItem
+    {
+        public int Id { get; set; }
+        public string RomajiScript { get; set; }
+        public string HiraganaScript { get; set; }
+        public string KatakanaScript { get; set; }
+    }
+}
