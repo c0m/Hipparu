881efe656155cc97f9447c2fc9c0e36e31180aed
c0m <61923663+c0m@users.noreply.github.com>
2021-08-13 04:54:12 +0100

    Add some buttons to main menu, attempting code behind

---
+++ Hipparu/Pages/Index.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace Hipparu.Pages
+{
+    public partial class Index
+    {
+        private int gameMode = 0;
+
+        void IncrementCount(int mode)
+        {
+            if (mode == 0)
+            {
+                gameMode++;
+            }
+            else
+            {
+                gameMode = 0;
+            }
+        }
+    }
+}
